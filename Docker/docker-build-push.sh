ee45f2af1ee09789a684d78b223b25321099f667
bilel <bilel.brahim@gmail.com>
2019-10-07 21:34:06 +0200

    refactoring

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -3,7 +3,7 @@
 # Usage (from root of project): sh ./Docker/docker-build-push.sh
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t bilel/storefront-fulfillment:gke-2.2.0 .
-docker push bilel/storefront-fulfillment:gke-2.2.0
+docker build -f Docker/Dockerfile --no-cache -t bilel/fulfillment:gke-2.2.0 .
+docker push bilel/fulfillment:gke-2.2.0
 
-# docker run --name storefront-fulfillment -d bilel/storefront-fulfillment:gke-2.2.0
+# docker run --name fulfillment -d bilel/fulfillment:gke-2.2.0
